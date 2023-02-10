48c01d379dd142f0ea265c0e7078e7ecb67f749e
Johann Mendoza <johannmendoza370@gmail.com>
2023-02-10 12:12:27 -0600

    initial commit

---
+++ src/main/java/com/example/chatappjavabackend/ChatAppJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatappjavabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppJavaBackendApplication.class, args);
+	}
+
+}
